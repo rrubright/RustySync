29e49e7f4b0d94f940539e3137503c04e3cb19a0
Richard Rubright <rrubright@gmail.com>
2026-08-17 11:30:08 -0500

    Make diskstats parsing directly testable

--- src/probe.rs
+++ src/probe.rs
@@ -27,7 +27,13 @@ pub fn probe(device: &DriveId) -> Result<DriveSample, Box<dyn std::error::Error>
 
 fn read_disk_stats(device: &str) -> Result<DiskStats, Box<dyn std::error::Error>> {
     let contents = fs::read_to_string("/proc/diskstats")?;
+    parse_disk_stats(&contents, device)
+}
 
+fn parse_disk_stats(
+    contents: &str,
+    device: &str,
+) -> Result<DiskStats, Box<dyn std::error::Error>> {
     let line = contents
         .lines()
         .find(|line| line.split_whitespace().nth(2) == Some(device))
@@ -40,4 +46,30 @@ fn read_disk_stats(device: &str) -> Result<DiskStats, Box<dyn std::error::Error>
         .collect::<Result<Vec<u64>, _>>()?;
 
     Ok(DiskStats { fields })
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parses_matching_device_stats() {
+        let contents =
+            "259 0 nvme0n1 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17\n";
+
+        let stats = parse_disk_stats(contents, "nvme0n1").unwrap();
+
+        assert_eq!(stats.fields[0], 1);
+        assert_eq!(stats.fields[4], 5);
+        assert_eq!(stats.fields[7], 8);
+    }
+
+    #[test]
+    fn missing_device_returns_error() {
+        let contents =
+            "259 0 nvme0n1 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17\n";
+
+        let result = parse_disk_stats(contents, "nvme9n9");
+
+        assert!(result.is_err());
+    }
 }
